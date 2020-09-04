4e6a7b0172abef720d60d9d86b9cb7ccbd399abf
Rutuja Kathavate <56214481+Rutujak139@users.noreply.github.com>
2020-09-04 15:03:27 +0530

    Add files via upload

---
+++ Day2_Assignment/search.js
@@ -0,0 +1,9 @@
+// Program for search for a particular character in string
+
+
+let string = "LetUpgrade..!";
+
+console.log("Position of character U is ",string.search(/U/));
+console.log("Position of character L is ",string.search(/L/));
+
+console.log("--------------------------");
