74a551dd40aa67a9cf4eba9d132c100a90e89565
Rutuja Kathavate <56214481+Rutujak139@users.noreply.github.com>
2020-09-04 14:17:12 +0530

    Delete search.js

--- search.js
+++
@@ -1,9 +0,0 @@
-// Program for search for a particular character in string
-
-
-let string = "LetUpgrade..!";
-
-console.log("Position of character U is ",string.search(/U/));
-console.log("Position of character L is ",string.search(/L/));
-
-console.log("--------------------------");
