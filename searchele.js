e123a3cae0206e57784005b56f9f0ed7b6059e1f
Rutuja Kathavate <56214481+Rutujak139@users.noreply.github.com>
2020-09-04 14:17:31 +0530

    Delete searchele.js

--- searchele.js
+++
@@ -1,7 +0,0 @@
-// Program to search for a elements in a array of strings
-
-let colors = ["Red","Yellow","Brown","Pink","Orange","Black"];
-
-console.log(colors[2]);
-
-console.log("--------------------------");
